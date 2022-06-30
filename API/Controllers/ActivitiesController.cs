b6a7904a2a9a4ad9e3729d90eb048a422532c2e3
maheshsahoo <maheswar.sahu@gmail.com>
2022-06-30 21:23:34 +0530

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,31 +2,54 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        public DataContext _context { get; }
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
+      
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+          //  return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivitity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            //return await _context.Activities.FindAsync(id);
+          //  return Ok();
+
+          return await Mediator.Send(new Details.Query{Id=id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivitity(Activity activity)
+        {
+          return Ok(await Mediator.Send(new Create.Command {Activity=activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivitity(Guid id, Activity activity)
+        {
+          activity.Id=id;
+         return Ok(await Mediator.Send(new Edit.Command {Activity=activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivitity(Guid id, Activity activity)
+        {
+          
+         return Ok(await Mediator.Send(new Delete.Command {Id=id}));
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -32,24 +36,8 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-             opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            
-            });
+            services.AddApplicationServices(_config);
 
-            services.AddCors(opt=>
-            {
-                opt.AddPolicy("CorsPolicy",policy=>
-                {
-                          policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("*");
-                });
-            }
-            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
